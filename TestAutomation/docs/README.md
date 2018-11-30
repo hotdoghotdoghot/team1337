90fc0bd1df660c2ba430f08915d3c32cd51ca12e
HoldenLucas <hopalucas@gmail.com>
2018-11-30 12:48:04 -0500

    we done boys

---
+++ TestAutomation/docs/README.md
@@ -0,0 +1,2 @@
+`pip install pyperclip`
+you must also use your package manager to install python3.7 or higher
--- TestAutomation/scripts/runAllTests.py
+++ TestAutomation/scripts/runAllTests.py
@@ -16,52 +16,53 @@ for the_file in os.listdir(folder):
     except Exception as e:
         print(e)
 
-results = (
-    "<html><table><tr><th>Case</th><th>Pass?</th><th>Oracle</th><th>Output</th></tr>"
-)
+results = "<html>\
+    <link rel='stylesheet' href='style.css'>\
+    <table><tr><th>Description</th><th>Case</th>\
+    <th>Pass?</th><th>Oracle</th><th>Output</th></tr>"
 
 for case in range(1, 26):
     print(f"Runing case {case}")
     execLoc = f"../testCasesExecutables/testCase{case:02}.py"
     oracleLoc = f"../oracles/testCase{case:02}Oracle.txt"
+    desLoc = f"../testCases/testCase{case:02}.txt"
+
+    # get the test description
+    with open(desLoc, "r") as dFile:
+        description = dFile.readline()
 
     # get the registry pattern for each file
-    oFile = open(oracleLoc, "r")
-    reg = oFile.readline().strip()
-    oFile.close()
+    with open(oracleLoc, "r") as oFile:
+        reg = oFile.readline().strip()
     pattern = re.compile(reg)
 
     # print each exec output to temp
     subprocess.run(["python3.7", execLoc])
-
     if case == 20:
-        tempFile1 = open(f"../temp/{case:02}-1.txt", "r")
-        tempFile2 = open(f"../temp/{case:02}-2.txt", "r")
-        testOutput1 = tempFile1.readline().strip()
-        testOutput2 = tempFile2.readline().strip()
-        tempFile1.close()
-        tempFile2.close()
+        with open(f"../temp/{case:02}-1.txt", "r") as tempFile1:
+            testOutput1 = tempFile1.readline().strip()
+        with open(f"../temp/{case:02}-2.txt", "r") as tempFile2:
+            testOutput2 = tempFile2.readline().strip()
         result1 = bool(pattern.match(testOutput1))
         result2 = bool(pattern.match(testOutput2))
         dontMatch = testOutput1 != testOutput2
         result = result1 and result2 and dontMatch
     else:
-        tempFile = open(f"../temp/{case:02}.txt", "r")
-        testOutput = tempFile.readline().strip()
-        tempFile.close()
+        with open(f"../temp/{case:02}.txt", "r") as tempFile:
+            testOutput = tempFile.readline().strip()
         result = bool(pattern.match(testOutput))
 
-    strRW = f"<tr>\
+    row = f"<tr>\
         <td>{case:02}</td>\
+        <td>{description}</td>\
         <td>{result}</td>\
         <td>{reg}</td>\
         <td>{testOutput}</td></tr>"
-    results = results + strRW
+    results = results + row
 results = results + "</table></html>"
 
 print("Writing and opening html...")
 resultsLoc = "results.html"
-hs = open(resultsLoc, "w")
-hs.write(results)
-# subprocess.run(["xdg-open", resultsLoc])
+with open(resultsLoc, "w") as hs:
+    hs.write(results)
 webbrowser.open(resultsLoc)
