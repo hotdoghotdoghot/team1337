00c7ab4270c649d9e99397e093d36ffc8a6400e0
HoldenLucas <hopalucas@gmail.com>
2018-11-30 04:47:25 -0500

    farts

--- TestAutomation/testCasesExecutables/testCase25.py
+++ TestAutomation/testCasesExecutables/testCase25.py
@@ -5,7 +5,6 @@ loc = "../project/packages/lesspass-cli/index.js"
 output = subprocess.check_output([loc, "test", "test", "test", "-C"])
 
 clipB = pyperclip.paste()
-print(clipB)
 
 f = open("../temp/25.txt", "w")
 f.write(clipB)
