70bafce3157aec0e4ea0b20f354415010d42115b
HoldenLucas <hopalucas@gmail.com>
2018-11-29 20:11:51 -0500

    test cases

--- TestAutomation/scripts/test.py
+++ TestAutomation/scripts/test.py
@@ -1,6 +1,7 @@
 import subprocess
+import pyperclip
 
 loc = "../project/packages/lesspass-cli/index.js"
-# output = subprocess.check_output(["echo", "Hello World!"])
 output = subprocess.check_output([loc, "test", "test", "test", "-lup"])
 print(str(output))
+print(pyperclip.paste())
